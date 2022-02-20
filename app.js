a81160a13e40b61c851f340465613bdb70a0d03f
AxithChoudhary <61582887+AxithChoudhary@users.noreply.github.com>
2022-02-20 23:55:58 +0530

    fourth commit

--- app.js
+++ app.js
@@ -2,20 +2,36 @@ let album
 let searchButton=document.querySelector("#button")
 let results
 let musciCard=document.querySelector(".musicCard")
-
+// let del=document.querySelector(".delete")
+// // let searchClick=false
 
 searchButton.addEventListener("click",async ()=>{
     album=await document.getElementById('input-song').value
-    // console.log(album)
-    const data=await fetch(`https://itunes.apple.com/search?term=${album}&media=music&limit=10.`)
+    const data=await fetch(`https://itunes.apple.com/search?term=${album}&media=music&limit=15.`)
     const music=await data.json()
     const results=music.results
     results.forEach(element => 
         {
-            // console.log(element)
-            let img = document.createElement("img");
+            console.log(element)
+            const divTwo=document.createElement("div");
+            divTwo.classList.add("cardBig");
+            const divOne=document.createElement("div");
+            divOne.classList.add("card");
+            const img = document.createElement("img");
             img.src = element.artworkUrl100;
-            musciCard.appendChild(img);
+            const link=document.createElement("a")
+            link.href=element.collectionViewUrl;
+            const name=document.createElement("p");
+            name.innerText=element.collectionName;
+            divOne.appendChild(img);
+            divOne.appendChild(name);
+            divTwo.appendChild(link);
+            link.appendChild(divOne)
+            
+            musciCard.appendChild(divTwo);
     });
+    // let clickOnMusic=document.querySelector(".cardBig")
+    // clickOnMusic.addEventListener("click",console.log(a.href))
+    // searchClick=true
 
 })
