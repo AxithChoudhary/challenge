1be7243f6f7cbfefb0319476f2601cb632185677
AxithChoudhary <61582887+AxithChoudhary@users.noreply.github.com>
2022-02-20 21:46:28 +0530

    third commi

--- app.js
+++ app.js
@@ -4,29 +4,18 @@ let results
 let musciCard=document.querySelector(".musicCard")
 
 
-
 searchButton.addEventListener("click",async ()=>{
-
-    album=await document.querySelector("#input-song").Value
-    const data=await fetch(`https://itunes.apple.com/search?term=${album}&media=music&limit=25.`)
+    album=await document.getElementById('input-song').value
+    // console.log(album)
+    const data=await fetch(`https://itunes.apple.com/search?term=${album}&media=music&limit=10.`)
     const music=await data.json()
     const results=music.results
-    if(results.length<=0){
-        results.forEach(element => 
-            {
-                let img = document.createElement("img");
-                img.src = results[i].artworkUrl100;
-                musciCard.appendChild(img);
-        });
-    }else{
-        for (let i = 0; i <= 5; i++)
-            {
-                console.log(results[i])
-                let img = document.createElement("img");
-                img.src = results[i].artworkUrl100;
-                musciCard.appendChild(img);
-    
-            } 
-    }
+    results.forEach(element => 
+        {
+            // console.log(element)
+            let img = document.createElement("img");
+            img.src = element.artworkUrl100;
+            musciCard.appendChild(img);
+    });
 
 })
